802297c1eb4103b0ebe84e9f9be6cddbf072ccea
Michael Collingwood <mike@fairways.me>
2020-07-06 14:51:32 +0100

    Initial edition

---
+++ Fizzbuzzprog/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace System
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                //Console.WriteLine(i);
+                if (i%3 == 0 && i%5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%3 != 0 && i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
